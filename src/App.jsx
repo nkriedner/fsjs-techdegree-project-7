5c95f7edcd76b663c6e8646f4e830abe98f77263
Nils Kriedner <nkriedner@googlemail.com>
2024-02-24 20:32:44 +0100

    Delete not needed Vite code and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import apiKey from "./config";
 import PhotoList from "./components/PhotoList";
 import Nav from "./components/Nav";
 import Search from "./components/Search";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <div className="container">
