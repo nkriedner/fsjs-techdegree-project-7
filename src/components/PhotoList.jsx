cf7bca0838b4446ad70193d6ca37bc5858e9da59
Nils Kriedner <nkriedner@googlemail.com>
2024-02-24 20:14:17 +0100

    Build components with given html mockup

---
+++ src/components/PhotoList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Photo from "./Photo";
+
+const PhotoList = () => {
+    return (
+        <div className="photo-container">
+            <h2>Results</h2>
+            <ul>
+                <Photo />
+                <Photo />
+                <Photo />
+                <Photo />
+
+                <li className="not-found">
+                    <h3>No Results Found</h3>
+                    <p>You search did not return any results. Please try again.</p>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default PhotoList;
